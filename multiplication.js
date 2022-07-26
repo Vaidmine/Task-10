c5764a4c294f1b9970d664e9c774bf4e1f900136
Vaidmine <vaidmine@gmail.com>
2022-07-26 19:54:06 +0300

    commit

---
+++ multiplication.js
@@ -0,0 +1,1 @@
+export let d = 5 * 2;
---
+++ script.js
@@ -0,0 +1,13 @@
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
+
+import { d } from "./multiplication.js";
+import { b } from "./division.js";
+import { c } from "./substraction.js";
+import { a } from "./composition.js";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
